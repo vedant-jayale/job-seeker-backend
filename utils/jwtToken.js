a25756f4f560747525d33a19d609ed7fdc47c9ef
Vedant Jayale <145029283+vedant-jayale@users.noreply.github.com>
2024-08-26 12:03:24 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -8,7 +8,7 @@ export const sendToken = (user, statusCode, res, message) => {
       expires: new Date(
         Date.now() + COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true, // Set httpOnly to true
+      httpOnly: true, secure: true, sameSite: 'None'
     };
   
     res.status(statusCode).cookie("token", token, options).json({
